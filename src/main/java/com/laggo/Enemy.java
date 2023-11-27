4ad12aabf937f0a887567e271568ee710c499186
Dante Dam <laggycomputer@yahoo.com>
2023-11-27 15:06:02 -0800

    dead imports

--- src/main/java/com/laggo/Enemy.java
+++ src/main/java/com/laggo/Enemy.java
@@ -6,9 +6,7 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Enemy extends BoardObject {
     private static final List<String> NAMES = new ArrayList<>(List.of(new String[]{
