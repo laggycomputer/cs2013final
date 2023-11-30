ae75bc9585bd70dda4bd9d4bb38d5554641a6e6e
Dante Dam <laggycomputer@yahoo.com>
2023-11-30 12:19:03 -0800

    rename main class

--- src/main/java/com/laggo/GameMain.java
+++ src/main/java/com/laggo/GameMain.java
@@ -1,12 +1,8 @@
 package com.laggo;
 
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 
-public class InputTest {
+public class GameMain {
     public static void main(String[] args) throws IOException {
         TerminalInterface terminalInterface = new TerminalInterface();
         // these dimensions leave room for the additional text pop-ups and account for the wide printing
